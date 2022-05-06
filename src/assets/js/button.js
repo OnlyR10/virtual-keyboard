55df8a5d26dbcf5d33f2672867cc14ceaa144994
Kirill Naydovich <kirillnaydovich@gmail.com>
2022-05-06 16:39:04 +0300

    init: start virtual keyboard task

---
+++ src/assets/js/button.js
@@ -0,0 +1,5 @@
+const button = document.createElement('div');
+button.classList.add('button');
+document.body.append(button);
+
+export default button;
---
+++ src/assets/js/index.js
@@ -0,0 +1,5 @@
+import button from './button';
+
+button.addEventListener('click', () => {
+  console.log('Hello');
+});
